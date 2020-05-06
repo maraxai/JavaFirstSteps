9d41376f1fa11472d772bacd69f52f9091ffeddd
Mike Dietz <mikedietz@hotmail.com>
2020-05-06 18:36:56 +0200

    2_Variables

---
+++ Variables.java
@@ -0,0 +1,50 @@
+public class Variables {
+	public static void main(String args[]) {
+
+	// the current world population
+	long worldPopulation = 7_781_516_554L;
+	System.out.println("The current world population: " + worldPopulation );
+	System.out.println();
+
+	// the brand of your favorite snack food
+	String favoriteSnack = "Chips";
+	System.out.println("My favorite snack food: " + favoriteSnack );
+	System.out.println();
+
+	// your age
+	byte age = 54;
+	System.out.println("My age: " + age );
+	System.out.println();
+
+	// the price of your favorite food per unit
+	float priceLaysChips = 0.79F;
+	System.out.println("The price for " + favoriteSnack + " is " + priceLaysChips + " EUR" );
+	System.out.println();
+
+	// the number Pi, as accurate as possible
+	double pi = 3.141592653589793;
+	System.out.println("Pi: " + pi );
+	System.out.println();
+
+	// the single-letter amino-acid code abbreviation for Glutamine
+	char Glutamine = 'Q';
+	System.out.println("single-letter code abbreviation for amino-acid Glutamine: " + Glutamine );
+	System.out.println();
+
+	// estimated annual expenses for food
+	short expensesFood = 2400;
+	System.out.println("estimated expenses for food: " + expensesFood + " EUR");
+	System.out.println();
+
+	// estimated distance to sun
+	int distanceSun = 150_000_000;	
+	System.out.println("The average, estimated distance to the sun is: " + distanceSun + " km" );
+	System.out.println();	
+
+	// does is currently rain
+	boolean rain = false;	
+	System.out.println("It rains! This statement is: " + rain );
+	System.out.println();	
+
+	}
+}
